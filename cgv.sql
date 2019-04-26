2e91f90e8a52522470aba55ae78b0f89819db554
wahyuhr18 <38802961+wahyuhr18@users.noreply.github.com>
2019-04-26 17:26:24 +0700

    website cgv.com
    - 16 halaman
    - crud belum jalan tepapi untuk join (registrasi) sudah dibuatkan model dan controller nya , tetapi jika di jalankan halaman registrasi akan -error dan jika di controller model tidak di panggil halaman registrasi jalan
    tampilan.
    -fungsi sudah sama
    -tampilan sudah sama

---
+++ cgv.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2019 at 07:34 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cgv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `billing_name` varchar(25) DEFAULT NULL,
+  `billing_address` varchar(30) DEFAULT NULL,
+  `billing_city` varchar(20) DEFAULT NULL,
+  `billing_country` varchar(20) NOT NULL,
+  `billing_telephone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
